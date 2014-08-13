7a3fef818cb185c00b403525a28b723d35e513a5
Max Burger <mburgerdesign@gmail.com>
2014-08-13 17:32:33 -0600

    fixed the about background problem

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-var sShow = i;
-var sshowarray = []
+//var sShow = i;
+//var sshowarray = []
+var images = ["img/_dsc0038.jpg", "img/_dsc0127.jpg", "img/_dsc0249.jpg", "img/_dsc0251.jpg", "img/_dsc0402.jpg", "img/_dsc0448.jpg"];
 
-
+            
+            // use a for loop with an if state ment in it for the slideshow.
+            
